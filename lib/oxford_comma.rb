90f5d35eb5b731ff7a708b956cf73ce1cbf12b6e
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-15 19:48:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  if array.length < 3
+  if array.length < 2
   array.join(" and ")
-elsif array.length >=3
-   array.to_sentence
+elsif array.length >=2
+   array[0..-2].join(', ') + " and " + array[-1]
   end
 end
