11923e6a673cef2d47d61df629a4241790f95104
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-15 19:48:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,6 @@ def oxford_comma(array)
   if array.length < 2
   array.join(" and ")
 elsif array.length >=2
-   array[0..-2].join(', ') + " and " + array[-1]
+   array[0..-2].join(', ') + " ,and " + array[-1]
   end
 end
